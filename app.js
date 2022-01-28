8c305a55f452d15e069c8433b612d160ea6f6c80
Mimsical <64818574+FriedFrenchFries@users.noreply.github.com>
2022-01-28 13:33:19 -0500

    Update app.js

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.listen(port);
 console.log("Server started at http://localhost:" + port);
 
 main().catch(console.error);
-await getKeys()
+getKeys()
 async function getKeys() {
     let keydoc = await keymodel.findOne({
         fillerfind: 0
