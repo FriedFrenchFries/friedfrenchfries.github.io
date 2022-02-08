562fc15c3a2ac2d09514075fa3cf7c070069822c
Mimsical <64818574+FriedFrenchFries@users.noreply.github.com>
2022-02-08 12:07:44 -0500

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const ssl = {
     key: fs.readFileSync(path.join(__dirname, '/ssl.key')),
     cert: fs.readFileSync(path.join(__dirname, '/ssl.cert')),
 };
-const server = https.createServer();
+const server = https.createServer(ssl);
 const proxy = new Corrosion({
     codec: 'xor', // apply basic xor encryption to url parameters in an effort to evade filters. Optional.
     prefix: '/get/' // specify the endpoint (prefix). Optional.
